a0ede80a8e623a29839852278a61d194041aa721
Eduard <sekaneduard@gmail.com>
2026-02-21 14:44:34 +0200

    add new feature sender for message

--- client/src/components/MessageBubble/messagebubble.tsx
+++ client/src/components/MessageBubble/messagebubble.tsx
@@ -1,12 +1,17 @@
 import type { Message } from "@/types/message"
 import styles from "@/components/messagebubble/messagebubble.module.scss"
+import { useAuth } from "@/hooks/useAuth"
 
 type MessageBubbleProps = {
   message: Message
 }
 
 export default function MessageBubble({ message }: MessageBubbleProps) {
-  const bubbleClassName = message.sender === "me" ? `${styles.bubble} ${styles.myBubble}` : styles.bubble
+  const { user } = useAuth()
+
+  const bubbleClassName = message.username === user?.username ? `${styles.bubble} ${styles.myBubble}` : styles.bubble
+  console.log(bubbleClassName);
+  
 
   return (
     <article className={bubbleClassName}>
