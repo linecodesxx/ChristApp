1e2ffc554622d67307431b84aa610fc9cc7a9c7e
linecodesxx <robloxgeroy09@gmail.com>
2026-03-04 16:20:43 +0400

    fix

--- frontend/src/app/bible/[book]/page.tsx
+++ frontend/src/app/bible/[book]/page.tsx
@@ -1,4 +1,4 @@
-import { getBibleData } from "@/lib/storage"
+// import { getBibleData } from "@/lib/storage"
 import styles from "./page.module.scss"
 import BibleReader from "@/components/BibleReader/BibleReader";
 
@@ -8,24 +8,24 @@ type Props = {
 };
 
 export default async function BookPage({ params }: Props) {
-  const { book } = await params;
-  const bible = await getBibleData();
-  const bookId = parseInt(book);
-  const selectedBook = bible.Books.find((item) => item.BookId === bookId);
+  // const { book } = await params;
+  // // const bible = await getBibleData();
+  // const bookId = parseInt(book);
+  // const selectedBook = bible.Books.find((item) => item.BookId === bookId);
 
-  if (!selectedBook) return <div>Книга не найдена</div>;
+  // if (!selectedBook) return <div>Книга не найдена</div>;
 
-  // parse optional chapter/verse from query
-  const chapterId = searchParams?.chapter ? parseInt(searchParams.chapter) : undefined;
-  const verseId = searchParams?.verse ? parseInt(searchParams.verse) : undefined;
+  // // parse optional chapter/verse from query
+  // const chapterId = searchParams?.chapter ? parseInt(searchParams.chapter) : undefined;
+  // const verseId = searchParams?.verse ? parseInt(searchParams.verse) : undefined;
 
   return (
     <main className={`${styles.main} container`}>
       <BibleReader
-        bible={bible}
-        initialBookId={bookId}
-        initialChapterId={chapterId}
-        initialVerseId={verseId}
+        // bible={bible}
+        // initialBookId={bookId}
+        // initialChapterId={chapterId}
+        // initialVerseId={verseId}
       />
     </main>
   );
