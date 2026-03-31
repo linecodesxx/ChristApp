761afd7857d7126762d332addf569bf6e43e15f0
Eduard <sekaneduard@gmail.com>
2026-03-31 22:21:49 +0300

    add fix for deploy

--- frontend/src/lib/storage.ts
+++ frontend/src/lib/storage.ts
@@ -1,7 +1,3 @@
-import { readFile } from "node:fs/promises";
-import path from "node:path";
-import type { BibleData } from "@/types/bible";
-
 // export async function getBibleData(): Promise<BibleData> {
 //   const jsonPath = path.join(process.cwd(), "public", "nrt.json");
 //   const content = await readFile(jsonPath, "utf-8");
